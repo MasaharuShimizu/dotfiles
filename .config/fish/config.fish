ba3eb72122ce0779192c1d43510829ba4edb654c
Shimizu Masaharu <ariaira08@gmail.com>
2015-02-22 15:51:22 +0900

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,34 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme zish
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins theme extract
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# terminal colors
+alias ls='gls -F --color=auto'
+alias la='gls -aF --color=auto'
+alias ll='gls -lF --color=auto'
+alias grep='grep --color=auto'
+set -x LS_COLORS (cat ~/dotfiles/.config/fish/dircolors.solarized-ansi-dark-fish)
+
+# ssh-agent
+ssh-add ~/.ssh/id_rsa_juno 
+ssh-add ~/.ssh/id_rsa_zero2
+ssh-add ~/.ssh/id_rsa_gitlab
+
+# environmental varaiables
+set PATH $PATH ~/bin ~/bin/dotfiles/bin
+
+# others
+alias rm='rmtrash'
