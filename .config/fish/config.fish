4202fe1f2599047cba0565e2f8f8208645348f8d
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2021-12-15 22:00:05 +0900

    Use exa instead of ls

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,20 @@
 # Delete messages when fish-shell starts
 set -u fish_greeting
 
-# terminal colors
-alias ls='ls -GF'
-alias la='ls -aGF'
-alias ll='ls -lGF'
-alias grep='grep --color=auto'
+# Use exa instead of ls
+if command -v exa > /dev/null
+  alias e='exa --icons'
+  alias l=e
+  alias ls=e
+  alias ea='exa -a --icons'
+  alias la=ea
+  alias ee='exa -aal --icons'
+  alias ll=ee
+  alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+  alias lt=et
+  alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+  alias lta=eta
+end
 
 # ssh-agent
 ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
@@ -16,7 +25,8 @@ set -x PATH $PATH /usr/local/sbin
 source ~/.env
 
 # Starship
-eval (starship init fish)
+starship init fish | source
+
 
 # Git
 set -x PATH $PATH /usr/local/share/git-core/contrib/diff-highlight
